2ef26c252dc0ca8925817d7481c014f629388694
pandeyshashidhar <91371891+pandeyshashidhar@users.noreply.github.com>
2026-03-28 19:19:17 +0530

    This is my first commit

---
+++ backEnd/src/main/java/com/myanatomy/sandboxpro/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.myanatomy.sandboxpro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot Backend!";
+    }
+
+    @GetMapping("/getAllHotelData")
+    public String getAllHotelData() {
+        return "This will give you all hotel details";
+    }
+}
